094e258ef17638c052ca9fddf66fa41fe85a8281
life light <lifelightdev@gmail.com>
2024-07-15 22:23:29 +0200

    Add Time

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import '@angular/common/locales/global/fr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    { provide: LOCALE_ID, useValue: 'fr-FR' },
+    provideRouter(routes)]
 };
--- src/app/now/now.component.ts
+++ src/app/now/now.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-now',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './now.component.html',
   styleUrl: './now.component.css'
 })
 export class NowComponent {
+  public clock = new Date();
+
+  constructor() {
+    setInterval(() => this.refresh(), 1000)
+  }
+
+  private refresh() {
+    this.clock = new Date();
+  }
 
 }
