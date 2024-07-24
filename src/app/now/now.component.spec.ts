c216c7271c6ebe4a003913920f6208c1aba22ea9
life light <lifelightdev@gmail.com>
2024-07-24 16:03:51 +0200

    Refactor

--- src/app/now/now.component.spec.ts
+++ src/app/now/now.component.spec.ts
@@ -2,8 +2,9 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {NowComponent} from './now.component';
 import {AppComponent} from "../app.component";
-import {SolarService, SunPosition} from '../solar.service';
+import {SolarService} from '../solar.service';
 import {of} from "rxjs";
+import {SunPosition} from '../sun-position';
 
 describe('NowComponent', () => {
   let component: NowComponent;
@@ -11,8 +12,9 @@ describe('NowComponent', () => {
   let solarServiceStub: Partial<SolarService>;
 
   beforeEach(async () => {
+    const sunsetDate = of(new SunPosition(new Date(2024, 6, 23, 21, 44)));
     solarServiceStub = {
-      findSunPositionAtEiffelTower: jasmine.createSpy('findSunPositionAtEiffelTower').and.returnValues(of(new SunPosition(new Date(2024, 6, 23, 41, 43))))
+      findSunPositionAtEiffelTower: jasmine.createSpy('findSunPositionAtEiffelTower').and.returnValue(sunsetDate)
     };
 
     await TestBed.configureTestingModule({
@@ -29,11 +31,26 @@ describe('NowComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render time', () => {
+  it('should render date and time now', () => {
+    const dateNow = new Date(2024, 6, 14, 18, 0)
+    jasmine.clock().mockDate(dateNow); // Il est 18h00 le 14 juillet 2024
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('#clock')?.textContent).toContain('il est ');
+    const result = dateNow.toLocaleDateString("en", {weekday: "long"}) + ' ' +
+      dateNow.getDate() + ' ' +
+      dateNow.toLocaleString("en", {month: "long"}) + ' ' +
+      dateNow.getFullYear() + ' et il est ' +
+      dateNow.getHours() + 'h' +
+      dateNow.getMinutes();
+    expect(compiled.querySelector('#clock')?.textContent).toContain('Nous sommes le ' + result);
+  });
+
+  it('should render time of sunset in the Eiffel tower', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('#sunset')?.textContent).toContain('Aujourd\'hui le soleil se couche à 21h44 sur la tour Eiffel');
   });
 
 });
--- src/app/solar.service.ts
+++ src/app/solar.service.ts
@@ -2,17 +2,18 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {parse} from 'date-fns';
 import {tap} from "rxjs";
+import {SunPosition} from './sun-position';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SolarService {
-
   constructor(private readonly http: HttpClient) {
   }
 
   public findSunPositionAtEiffelTower() {
-    return this.http.get<SunPosition>(`https://api.sunrise-sunset.org/json?lat=48.858370&lng=2.294481`).pipe(tap(data => {
+    const url = `https://api.sunrise-sunset.org/json?lat=48.858370&lng=2.294481`;
+    return this.http.get<SunPosition>(url).pipe(tap(data => {
       if (data.results?.sunset) {
         const sunsetDateParse: Date = parse(data.results.sunset.toString(), 'h:mm:ss a', new Date());
         let sunsetDate = new Date();
@@ -23,29 +24,4 @@ export class SolarService {
       }
     }));
   }
-
-}
-
-export class SunPosition {
-  results: Results | undefined;
-  status: string | undefined;
-  tzid: string | undefined;
-
-  constructor(sunset?: Date) {
-    this.results = new Results();
-    this.results.sunset = sunset;
-  }
-}
-
-export class Results {
-  sunrise: Date | undefined;
-  sunset: Date | undefined;
-  solar_noon: Date | undefined;
-  day_length: number | undefined;
-  civil_twilight_begin: Date | undefined;
-  civil_twilight_end: Date | undefined;
-  nautical_twilight_begin: Date | undefined;
-  nautical_twilight_end: Date | undefined;
-  astronomical_twilight_begin: Date | undefined;
-  astronomical_twilight_end: Date | undefined;
 }
